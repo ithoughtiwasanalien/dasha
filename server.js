bd9c6198ef412619f8ddcbc94cc2807638d5e190
daria <daryakasyan@gmail.com>
2019-12-15 21:46:45 +0200

    commit frm me

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-<<<<<<< HEAD
 app.get('/', (req, res) => res.send('Hello Dasha! (from Dasha and masha) ?????'))
-=======
-app.get('/', (req, res) => res.send('Hello Dasha! (from Dasha) asdasdasd?????'))
->>>>>>> 845c8200adf7d9f48cf6f6da4ecec7fef0b3ee67
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
